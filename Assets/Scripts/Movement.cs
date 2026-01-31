7c04c797e25b23edcf716ee04f4b63c57d0f52a5
Isaac Rojas <stevenisaac3103@gmail.com>
2026-01-31 11:49:28 -0300

    Movimiento del  Player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float baseSpeed = 4f;
+    [SerializeField] private float iceSpeedMultiplier = 1.5f;
+    [SerializeField] private float wallSlideSpeed = 2f;
+    [SerializeField] private float wallJumpForce = 15f;
+
+    private float currentSpeed;
+    private bool onIceFloor = false;
+    private bool isTouchingWall = false;
+    private bool isWallSliding = false;
+    private bool jump;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        //First comment
+        rb = GetComponent<Rigidbody2D>();
+        currentSpeed = baseSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        CheckWallSliding();
+    }
+
+    void FixedUpdate()
+    {
+        float speed = onIceFloor ? baseSpeed * iceSpeedMultiplier : currentSpeed;
+
+        // Movimiento constante hacia la derecha
+        rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+
+        if (jump && isWallSliding)
+        {
+            WallJump();
+        }
+
+        jump = false;
+    }
+
+    private void CheckWallSliding()
+    {
+        if (isTouchingWall && rb.linearVelocity.y < 0)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, -wallSlideSpeed);
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void WallJump()
+    {
+        rb.linearVelocity = new Vector2(-baseSpeed, wallJumpForce);
+        isWallSliding = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("iceFloor"))
+            onIceFloor = true;
+
+        if (collision.gameObject.CompareTag("Wall"))
+            isTouchingWall = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("iceFloor"))
+            onIceFloor = false;
+
+        if (collision.gameObject.CompareTag("Wall"))
+            isTouchingWall = false;
     }
 }
