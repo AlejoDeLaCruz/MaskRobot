a258c1b8928b0b2b48b6cc9b54bab68183fef31f
Isaac <stevenisaac3103@gmail.com>
2026-02-01 12:46:11 -0300

    Menu y Sprites

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
